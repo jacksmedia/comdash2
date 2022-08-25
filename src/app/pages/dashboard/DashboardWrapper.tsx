259e0b4c33a8921a971634c67b96060f9e0bc3c9
jacksmedia <alex@j4cks.com>
2022-08-24 23:12:42 -0700

    :construction: dashboard layout underway

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,21 +1,12 @@
 import {useIntl} from 'react-intl'
 import {PageTitle} from '../../../_metronic/layout/core'
 import {
-  MixedWidget2,
-  MixedWidget10,
-  MixedWidget11,
-  ListsWidget2,
-  ListsWidget3,
   ListsWidget4,
-  ListsWidget5,
-  ListsWidget6,
-  TablesWidget10,
   MixedWidget8,
   TilesWidget1,
   TilesWidget4,
   TilesWidget2,
   TilesWidget5,
-  TilesWidget3,
 } from '../../../_metronic/partials/widgets'
 
 const DashboardPage = () => (
@@ -30,30 +21,12 @@ const DashboardPage = () => (
       </div>
 
       <div className='col-xl-8'>
-        <div className='row gx-5 gx-xl-8 mb-5 mb-xl-8'>
-          <div className='col-xl-3'>
-            <TilesWidget1 className='card-xl-stretch' />
-          </div>
-          <div className='col-xl-9'>
-            <TilesWidget4 className='card-xl-stretch' />
-          </div>
-        </div>
+        
 
         <div className='row gx-5 gx-xl-8 mb-5 mb-xl-8'>
           <div className='col-xl-6'>
             <TilesWidget2 className='card-xl-stretch mb-5 mb-xl-8' />
             <div className='row gx-5 gx-xl-8'>
-              <div className='col-xl-6'>
-                <TilesWidget5
-                  className='card-xxl-stretch bg-primary'
-                  svgIcon='/media/icons/duotune/general/gen025.svg'
-                  titleClass='text-white'
-                  descriptionClass='text-white'
-                  iconClass='svg-icon-white'
-                  title='790'
-                  description='New Products'
-                />
-              </div>
               <div className='col-xl-6'>
                 <TilesWidget5
                   className='card-xxl-stretch bg-body'
@@ -67,57 +40,17 @@ const DashboardPage = () => (
               </div>
             </div>
           </div>
-          <div className='col-xl-6'>
-            <TilesWidget3 className='card-xl-stretch mb-5 mb-xl-8' />
-          </div>
+         
         </div>
       </div>
     </div>
 
-    {/* begin::Row */}
-    <div className='row gy-5 g-xl-8'>
-      <div className='col-xxl-4'>
-        <MixedWidget2
-          className='card-xl-stretch mb-xl-8'
-          chartColor='danger'
-          chartHeight='200px'
-          strokeColor='#cb1e46'
-        />
-      </div>
-      <div className='col-xxl-4'>
-        <ListsWidget5 className='card-xxl-stretch' />
-      </div>
-      <div className='col-xxl-4'>
-        <MixedWidget10
-          className='card-xxl-stretch-50 mb-5 mb-xl-8'
-          chartColor='primary'
-          chartHeight='150px'
-        />
-        <MixedWidget11
-          className='card-xxl-stretch-50 mb-5 mb-xl-8'
-          chartColor='primary'
-          chartHeight='175px'
-        />
-      </div>
-    </div>
-    {/* end::Row */}
+    
 
-    {/* begin::Row */}
-    <div className='row gy-5 gx-xl-8'>
-      <div className='col-xxl-4'>
-        <ListsWidget3 className='card-xxl-stretch mb-xl-3' />
-      </div>
-      <div className='col-xl-8'>
-        <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' />
-      </div>
-    </div>
-    {/* end::Row */}
+
 
     {/* begin::Row */}
     <div className='row gy-5 g-xl-8'>
-      <div className='col-xl-4'>
-        <ListsWidget2 className='card-xl-stretch mb-xl-8' />
-      </div>
       <div className='col-xl-4'>
         <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} />
       </div>
