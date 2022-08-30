947bf94adbfcddc5d0c5e2f653d98bfe45531789
jacksmedia <alex@j4cks.com>
2022-08-29 22:44:22 -0700

    :wrench: slightly improved dashboard top panel UI

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -11,15 +11,14 @@ const DashboardPage = () => (
   <>
     {/* begin::Row */}
     <div className='row g-5 g-xl-8 mb-5'>
-      <div className='col-xl-4'>
-        <div className='card-xl-stretch mb-5 mb-xl-8 bg-success rounded d-flex flex-row flex-center' >
-          <div className='me-2'>
-            <h2 className='fw-bold text-gray-800 mb-3'>
-              Hello, @joeiwu
-            </h2>
-          </div>
+
+      <div className='col-xl-4 tile8-parent'>
+        <div className='tile8-div1 rounded bg-success d-flex flex-row flex-center card-xl-stretch mb-5 mb-xl-8'>
+          <h2 className='fw-bold text-gray-800 my-3'>Hello, @joeiwu</h2>
+        </div>
+        <div className='tile8-div2 rounded bg-body d-flex flex-row flex-center card-xl-stretch mb-5 mb-xl-8'> </div>
           <TilesWidget5
-            className='card-xxl-stretch bg-body mx-3 my-2 dark-card-bg'
+            className='bg-body mx-3 my-2 dark-card-bg tile8-div3'
             svgIcon='/media/svg/misc/cd-lines.svg'
             titleClass='text-dark'
             descriptionClass='text-muted'
@@ -28,16 +27,16 @@ const DashboardPage = () => (
             description='Tier Score'
           />
           <TilesWidget5
-            className='card-xxl-stretch bg-body mx-3 my-2 dark-card-bg'
+            className='bg-body mx-3 my-2 dark-card-bg tile8-div4'
             svgIcon='/media/svg/misc/cd-boxes.svg'
             titleClass='text-dark'
             descriptionClass='text-muted'
             iconClass='svg-icon-success'
             title='98'
             description='Combased NFTs'
-          />
-        </div>
+          />              
       </div>
+
       <div className='col-xl-8'>
         <TilesWidget3 className='card-xl-stretch mb-5 mb-xl-8 bg-body' />
       </div>
